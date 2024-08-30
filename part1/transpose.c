1046f1426deefb565f66020d103c802e46fa3092
Saimapath <140066295+Saimapath@users.noreply.github.com>
2024-08-30 16:34:24 +0530

    Update transpose.c

--- part1/transpose.c
+++ part1/transpose.c
@@ -34,17 +34,15 @@ void naiveMatrixTranspose(double *matrix, double *transpose, int size) {
 
 // Cache-Aware tiled Matrix Transpose
 void tiledMatrixTranspose(double *matrix, double *transpose, int size, int blockSize) {
-
-     for (int i = 0; i < size; i+=blockSize ){
+for (int i = 0; i < size; i+=blockSize ){
         for (int j = 0; j < size; j+=blockSize) {
-             for (int i1 = i; i1 < i+blockSize && i1<size; i++) {
-                    for (int j1 = j; j < j+blockSize && j1<size; j++) {
-                        transpose[j1 * blockSize + i] = matrix[i1 * blockSize + j];
+             for (int i1 = i; i1 < i+blockSize && i1<size; i1++) {
+                    for (int j1 = j; j1 < j+blockSize && j1<size; j1++) {
+                        transpose[i1 * size + j1] = matrix[j1 * size + i1];
                     }
                 }       
         }
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
     // Students need to implement this function
 }
 
